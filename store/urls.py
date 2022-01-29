e404be2bbfb280d81bf608371143ee71561270a6
Abhishek Kumar <abhishekk580@gmail.com>
2022-01-29 14:43:56 +0530

    added category url

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('products/', views.ListProductAPIView.as_view()),
     path('products/<int:pk>/', views.ProductAPIView.as_view()),
     path('categories/', views.ListCategoryAPIView.as_view()),
+    path('categories/<int:pk>/', views.CategoryAPIView.as_view()),
     path('products/category/<str:category>/',
          views.ProductsByCategory.as_view()),
 ]
